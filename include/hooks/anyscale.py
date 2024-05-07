8b5a5f6505675a9c6d42878f4a3e9c0d39fa4a94
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-05-07 17:52:27 -0400

    updated logging

--- include/hooks/anyscale.py
+++ include/hooks/anyscale.py
@@ -65,13 +65,9 @@ class AnyscaleHook(BaseHook):
         return prod_job
     
     def fetch_production_job_logs(self, job_id: str):
-        logs_list = []
         sdk = self.get_sdk()
         logs = sdk.fetch_production_job_logs(job_id = job_id)
-        if len(logs)>0:
-            for line in logs.split("\n"):
-                logs_list.append(line)
-        return logs_list
+        return logs
     
     def terminate_job(self, job_id: str):
         sdk = self.get_sdk()
