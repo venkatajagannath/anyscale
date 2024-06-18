d59a614c737d803224b1ee04cc03abe8dea2a95f
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-06-18 15:51:49 -0400

    service file updated

--- dags/anyscale_service.py
+++ dags/anyscale_service.py
@@ -1,45 +1,56 @@
-
 from datetime import datetime, timedelta
 from airflow import DAG
-
+from airflow.operators.python import PythonOperator
 from anyscale_provider.operators.anyscale import RolloutAnyscaleService
+from anyscale_provider.hooks.anyscale import AnyscaleHook
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 4, 2),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 4, 2),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 # Define the Anyscale connection
 ANYSCALE_CONN_ID = "anyscale_conn"
 
 dag = DAG(
-    'anyscale_service_workflow',
+    "sample_anyscale_service_workflow",
     default_args=default_args,
-    description='A DAG to interact with Anyscale triggered manually',
+    description="A DAG to interact with Anyscale triggered manually",
     schedule_interval=None,  # This DAG is not scheduled, only triggered manually
     catchup=False,
 )
 
 deploy_anyscale_service = RolloutAnyscaleService(
     task_id="rollout_anyscale_service",
-    conn_id = ANYSCALE_CONN_ID,
+    conn_id=ANYSCALE_CONN_ID,
     name="AstroService",
-    image_uri = 'anyscale/ray:2.23.0-py311',
-    compute_config = 'my-compute-config:1',
-    working_dir = "https://github.com/anyscale/docs_examples/archive/refs/heads/main.zip",
-    applications = [
-        {"import_path": "sentiment_analysis.app:model"}
-    ],
-    requirements = ["transformers","requests","pandas","numpy","torch"],
+    image_uri="anyscale/ray:2.23.0-py311",
+    compute_config="my-compute-config:1",
+    working_dir="https://github.com/anyscale/docs_examples/archive/refs/heads/main.zip",
+    applications=[{"import_path": "sentiment_analysis.app:model"}],
+    requirements=["transformers", "requests", "pandas", "numpy", "torch"],
     in_place=False,
-    canary_percent= None,
-    dag=dag
+    canary_percent=None,
+    dag=dag,
+)
+
+
+def terminate_service():
+    hook = AnyscaleHook(conn_id=ANYSCALE_CONN_ID)
+    result = hook.terminate_service(service_id="AstroService", time_delay=5)
+    print(result)
+
+
+terminate_anyscale_service = PythonOperator(
+    task_id="initialize_anyscale_hook",
+    python_callable=terminate_service,
+    dag=dag,
 )
 
 # Defining the task sequence
-deploy_anyscale_service
+deploy_anyscale_service >> terminate_anyscale_service
