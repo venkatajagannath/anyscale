0a2ca76bb7f868190d7217b8f426782b9589883a
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-06-24 14:34:21 -0400

    update

--- dags/anyscale_dag.py
+++ dags/anyscale_dag.py
@@ -1,41 +1,32 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from anyscale_provider.operators.anyscale import SubmitAnyscaleJob
+from datetime import datetime, timedelta
 from pathlib import Path
 
+from airflow import DAG
 
-def task1(params, ti, **kwargs):
-    # Accessing parameters from DAG
-    param1 = params["param1"]
-    param2 = params["param2"]
+from anyscale_provider.operators.anyscale import SubmitAnyscaleJob
 
-    # Do some processing with the parameters
-    result = param1 + param2
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 4, 2),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
 
-    # Passing the result to the next task
-    ti.xcom_push(key="result", value=result)
-
-
-# Define the DAG
-dag = DAG(
-    "dynamic_params_example",
-    description="Example DAG showcasing passing parameters and data dynamically",
-    schedule_interval=None,
-    start_date=datetime(2022, 1, 1),
-    catchup=False,
-    params={"param1": 10, "param2": 20},
-)
-
+# Define the Anyscale connection
 ANYSCALE_CONN_ID = "anyscale_conn"
+
+# Constants
 FOLDER_PATH = Path(__file__).parent / "ray_scripts"
 
-# Define the tasks
-task_1 = PythonOperator(
-    task_id="task1",
-    python_callable=task1,
-    op_kwargs={'params': dag.params},
-    dag=dag
+dag = DAG(
+    "sample_anyscale_job_workflow",
+    default_args=default_args,
+    description="A DAG to interact with Anyscale triggered manually",
+    schedule=None,  # This DAG is not scheduled, only triggered manually
+    catchup=False,
 )
 
 submit_anyscale_job = SubmitAnyscaleJob(
@@ -43,9 +34,9 @@ submit_anyscale_job = SubmitAnyscaleJob(
     conn_id=ANYSCALE_CONN_ID,
     name="AstroJob",
     image_uri="anyscale/ray:2.23.0-py311",
-    compute_config="airflow-integration-testing:1",
+    compute_config="my-compute-config:1",
     working_dir=str(FOLDER_PATH),
-    entrypoint="python script.py --param1 {{ params.param1 }} --param2 {{ params.param2 }} --result {{ ti.xcom_pull(task_ids='task1', key='result') }}",
+    entrypoint="python ray-job.py",
     requirements=["requests", "pandas", "numpy", "torch"],
     max_retries=1,
     job_timeout_seconds=3000,
@@ -53,5 +44,6 @@ submit_anyscale_job = SubmitAnyscaleJob(
     dag=dag,
 )
 
-# Define the task dependencies
-task_1 >> submit_anyscale_job
+
+# Defining the task sequence
+submit_anyscale_job
